4c0187148b5b6451a69152c7f9c985cb81e700a3
jonat <jonat@Jons-PC>
2024-09-24 15:44:16 -0700

    second commit

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -2,6 +2,7 @@ package test;
 
 public class test {
 	public static void main(String args[]) {
-		System.out.println("hello");
+		System.out.println("In Class Github Activity");
+		System.out.println("Jonathan's second commitS");
 	}
 }
